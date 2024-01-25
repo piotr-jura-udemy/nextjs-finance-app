421734a01fe0ab3d8733a2f4e46d7a7f3fba7fc7
Piotr Jura <jura.piotrek@gmail.com>
2024-01-25 18:13:27 +0100

    200-create-finance-app

---
+++ app/playground/page.js
@@ -0,0 +1,13 @@
+export default function Page() {
+  return (
+    <main className="space-y-8">
+      <h1 className="text-4xl mt-8">Playground</h1>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">ComponentName</h2>
+        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <div>Here goes the actual component</div>
+      </div>
+    </main>
+  )
+}
