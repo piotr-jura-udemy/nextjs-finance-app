e2a004116a040800d89ad0c3ac54b12a85c8f619
Piotr Jura <jura.piotrek@gmail.com>
2024-01-28 13:39:30 +0100

    206-transaction-summary-item

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,6 +1,7 @@
 import Trend from '@/components/trend'
 import PageHeader from '@/components/page-header'
 import TransactionItem from '@/components/transaction-item';
+import TransactionSummaryItem from '@/components/transaction-summary-item';
 
 export default function Page() {
   return (
@@ -28,6 +29,19 @@ export default function Page() {
         <h2 className="mb-4 text-lg font-mono">TransactionItem</h2>
         <hr className="mb-4 border-gray-200 dark:border-gray-800" />
         <div className="space-y-4">
+          <TransactionItem type="Income" description="Salary" amount={2000} />
+          <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
+          <TransactionItem type="Saving" description="For children" amount={500} />
+          <TransactionItem type="Investment" description="In Microsoft" amount={9000} />
+        </div>
+      </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">TransactionSummaryItem + TransactionItem</h2>
+        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <div className="space-y-4">
+          <TransactionSummaryItem date="2024-05-01" amount={3500} />
+          <hr className="mb-4 border-gray-200 dark:border-gray-800" />
           <TransactionItem type="Income" description="Salary" amount={2000} />
           <TransactionItem type="Expense" category="Food" description="Going out to eat" amount={29} />
           <TransactionItem type="Saving" description="For children" amount={500} />
---
+++ components/transaction-summary-item.js
@@ -0,0 +1,13 @@
+import { useFormatCurrency } from "@/hooks/use-format-currency"
+
+export default function TransactionSummaryItem({date, amount}) {
+  const formattedAmount = useFormatCurrency(amount)
+  return (<div className="flex text-gray-500 dark:text-gray-400 font-semibold">
+    <div className="grow">
+      {date}
+    </div>
+
+    <div className="min-w-[70px] text-right font-semibold">{formattedAmount}</div>
+    <div className="min-w-[50px]"></div>
+  </div>)
+}
