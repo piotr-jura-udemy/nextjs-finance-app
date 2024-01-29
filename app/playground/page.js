5a467de59107126284346cbd96e2e33e595009d4
Piotr Jura <jura.piotrek@gmail.com>
2024-01-29 19:33:11 +0100

    208-forms

--- app/playground/page.js
+++ app/playground/page.js
@@ -63,6 +63,32 @@ export default function Page() {
           <Button size="lg">Hello</Button>
         </div>
       </div>
+
+      <div>
+        <h2 className="mb-4 text-lg font-mono">Forms</h2>
+        <hr className="mb-4 border-gray-200 dark:border-gray-800" />
+        <div className="grid grid-cols-2 gap-4">
+          <div>
+            <label className="text-gray-700 dark:text-gray-300 block mb-1">Your name</label>
+            <input type="text" placeholder="Type something in here..." className="w-full rounded-md shadow-sm border-gray-300 bg-white dark:border-gray-700 dark:bg-gray-950" />
+          </div>
+
+          <div>
+            <label className="text-gray-700 dark:text-gray-300 block mb-1">City</label>
+            <select className="w-full rounded-md shadow-sm border-gray-300 bg-white dark:border-gray-700 dark:bg-gray-950">
+              <option>Warsaw</option>
+              <option>Berlin</option>
+              <option>London</option>
+            </select>
+          </div>
+
+          <div className="flex items-center">
+            <input type="checkbox" className="rounded border-gray-300 text-gray-700 bg-white dark:bg-gray-950 dark:text-gray-500 shadow-sm" />
+
+            <label className="text-gray-700 dark:text-gray-300 ml-2">City</label>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
