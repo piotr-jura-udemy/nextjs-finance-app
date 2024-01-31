c29f83368cede58a385fdcd16fdfb93b046e330a
Piotr Jura <jura.piotrek@gmail.com>
2024-01-31 21:43:00 +0100

    223-transaction-grouping

--- app/dashboard/components/transaction-list.js
+++ app/dashboard/components/transaction-list.js
@@ -1,14 +1,41 @@
 import TransactionItem from "@/components/transaction-item"
+import TransactionSummaryItem from "@/components/transaction-summary-item"
+
+const groupAndSumTransactionsByDate = (transactions) => {
+  const grouped = {}
+  for (const transaction of transactions) {
+    const date = transaction.created_at.split('T')[0]
+    if(!grouped[date]) {
+      grouped[date] = {transactions: [], amount: 0}
+    }
+    grouped[date].transactions.push(transaction)
+    const amount = transaction.type === 'Expense' ? -transaction.amount : transaction.amount
+    grouped[date].amount += amount
+  }
+  return grouped
+}
 
 export default async function TransactionList() {
   const response = await fetch(
     'http://localhost:3100/transactions'
   )
   const transactions = await response.json()
+  const grouped = groupAndSumTransactionsByDate(transactions)
   
-  return (<section className="space-y-4">
-    {transactions.map(transaction => <div key={transaction.id}>
-      <TransactionItem type={transaction.type} category={transaction.category} description={transaction.description} amount={transaction.amount} />
-    </div>)}
-  </section>)
+  return (
+    <div className="space-y-8">
+      {Object.entries(grouped)
+        .map(([date, { transactions, amount }]) =>
+          <div key={date}>
+            <TransactionSummaryItem date={date} amount={amount} />
+            <hr className="my-4 border-gray-200 dark:border-gray-800" />
+            <section className="space-y-4">
+              {transactions.map(transaction => <div key={transaction.id}>
+                <TransactionItem {...transaction} />
+              </div>)}
+            </section>
+          </div>
+        )}
+    </div>
+  )
 }
