38d187d5919635ff58f587621f25e5a7abb4a0ad
Piotr Jura <jura.piotrek@gmail.com>
2024-02-05 19:50:16 +0100

    240-add-transaction-page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,9 +3,16 @@ import TransactionList from "./components/transaction-list"
 import TransactionListFallback from "./components/transaction-list-fallback"
 import Trend from "./components/trend"
 import TrendFallback from "./components/trend-fallback"
+import Link from 'next/link'
+import {PlusCircle} from 'lucide-react'
+import { sizes, variants } from "@/lib/variants"
 
 export default function Page() {
   return (<>
+    <section className="mb-8">
+      <h1 className="text-4xl font-semibold">Summary</h1>
+    </section>
+
     <section className="mb-8 grid grid-cols-2 lg:grid-cols-4 gap-8">
       <Suspense fallback={<TrendFallback />}>
         <Trend type="Income" />
@@ -19,6 +26,14 @@ export default function Page() {
       <Suspense fallback={<TrendFallback />}>
         <Trend type="Investment" />
       </Suspense>
+    </section>
+
+    <section className="flex justify-between items-center mb-8">
+      <h2 className="text-2xl">Transactions</h2>
+      <Link href="/dashboard/transaction/add" className={`flex items-center space-x-1 ${variants['outline']} ${sizes['sm']}`}>
+        <PlusCircle className="w-4 h-4" />
+        <div>Add</div>
+      </Link>
     </section>
 
     <Suspense fallback={<TransactionListFallback />}>
---
+++ app/dashboard/transaction/add/page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <>
+    <h1 className="text-4xl font-semibold mb-8">Add Transaction</h1>
+  </>
+}
