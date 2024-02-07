0e4f2ef22144dbb5722a76686bcc8af7fc6daa29
Piotr Jura <jura.piotrek@gmail.com>
2024-02-07 21:23:35 +0100

    243-form-validation

--- app/dashboard/components/transaction-form.js
+++ app/dashboard/components/transaction-form.js
@@ -12,39 +12,57 @@ export default function TransactionForm() {
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm()
+  } = useForm({
+    mode: "onTouched"
+  })
 
   const onSubmit = (data) => console.log(data)
 
-  return <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+  return <form className="space-y-4"
+    onSubmit={handleSubmit(onSubmit)}>
     <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
       <div>
         <Label className="mb-1">Type</Label>
         <Select {...register("type")}>
-          {types.map(type => <option key={type}>{type}</option>)}
+          {types.map(type => <option key={type}>
+            {type}
+          </option>)}
         </Select>
       </div>
 
       <div>
         <Label className="mb-1">Category</Label>
         <Select {...register("category")}>
-          {categories.map(category => <option key={category}>{category}</option>)}
+          {categories.map(category => <option key={category}>
+            {category}
+          </option>)}
         </Select>
       </div>
 
       <div>
         <Label className="mb-1">Date</Label>
-        <Input {...register("created_at")} />
+        <Input {...register("created_at", {
+          required: "The date is required"
+        })} />
+        {errors.created_at && <p className="mt-1 text-red-500">{errors.created_at.message}</p>}
       </div>
 
       <div>
         <Label className="mb-1">Amount</Label>
-        <Input type="number" {...register("amount")} />
+        <Input type="number" {...register("amount", {
+          required: "The amount is required",
+          valueAsNumber: true,
+          min: {value: 1, message: "Amount must be at least 1"}
+        })} />
+        {errors.amount && <p className="mt-1 text-red-500">{errors.amount.message}</p>}
       </div>
 
-      <div className="col-span-2">
+      <div className="col-span-1 md:col-span-2">
         <Label className="mb-1">Description</Label>
-        <Input {...register("description")} />
+        <Input {...register("description", {
+          required: "The description is required"
+        })} />
+        {errors.description && <p className="mt-1 text-red-500">{errors.description.message}</p>}
       </div>
     </div>
 
