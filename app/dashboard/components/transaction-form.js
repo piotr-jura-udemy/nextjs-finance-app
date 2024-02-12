dee2a1d1943bd15b99e6317ecc7008dbeac5266a
Piotr Jura <jura.piotrek@gmail.com>
2024-02-12 20:52:24 +0100

    247-storing-data

--- app/dashboard/components/transaction-form.js
+++ app/dashboard/components/transaction-form.js
@@ -7,6 +7,7 @@ import { categories, types } from "@/lib/consts";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { transactionSchema } from "@/lib/validation";
+import { useState } from "react";
 
 export default function TransactionForm() {
   const {
@@ -19,9 +20,24 @@ export default function TransactionForm() {
     resolver: zodResolver(transactionSchema)
   })
 
-  const onSubmit = (data) => {
-    console.log(data)
-    console.log(process.env.NEXT_PUBLIC_API_URL)
+  const [isSaving, setSaving] = useState(false)
+
+  const onSubmit = async (data) => {
+    setSaving(true)
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_API_URL}/transactions`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          ...data,
+          created_at: `${data.created_at}T00:00:00`
+        })
+      })
+    } finally {
+      setSaving(false)
+    }
   }
 
   return <form className="space-y-4"
@@ -65,7 +81,7 @@ export default function TransactionForm() {
     </div>
 
     <div className="flex justify-end">
-      <Button type="submit">Save</Button>
+      <Button type="submit" disabled={isSaving}>Save</Button>
     </div>
   </form>
 }
