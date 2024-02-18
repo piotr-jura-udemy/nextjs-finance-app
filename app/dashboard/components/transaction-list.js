6048d54376935ef28c46bfd96550b8f788a120c2
Piotr Jura <jura.piotrek@gmail.com>
2024-02-18 18:40:50 +0100

    273-fetching-data-from-supabase

--- app/dashboard/components/transaction-list.js
+++ app/dashboard/components/transaction-list.js
@@ -1,6 +1,7 @@
 import Separator from "@/components/separator"
 import TransactionItem from "@/components/transaction-item"
 import TransactionSummaryItem from "@/components/transaction-summary-item"
+import { createClient } from "@/lib/supabase/server"
 
 const groupAndSumTransactionsByDate = (transactions) => {
   const grouped = {}
@@ -17,15 +18,11 @@ const groupAndSumTransactionsByDate = (transactions) => {
 }
 
 export default async function TransactionList() {
-  const response = await fetch(
-    `${process.env.API_URL}/transactions`,
-    {
-      next: {
-        tags: ['transaction-list']
-      }
-    }
-  )
-  const transactions = await response.json()
+  const supabase = createClient()
+  const {data: transactions, error} = await supabase
+    .from('transactions')
+    .select('*')
+    .order('created_at', {ascending: true})
   const grouped = groupAndSumTransactionsByDate(transactions)
   
   return (
