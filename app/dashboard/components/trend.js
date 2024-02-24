8133792a0cf0b8830213a98d6524704ba175b67b
Piotr Jura <jura.piotrek@gmail.com>
2024-02-24 15:55:19 +0100

    282-supabase-rpc

--- app/dashboard/components/trend.js
+++ app/dashboard/components/trend.js
@@ -1,7 +1,14 @@
 import BaseTrend from "@/components/trend"
+import { createClient } from "@/lib/supabase/server"
 
 export default async function Trend({type}) {
-  const response = await fetch(`${process.env.API_URL}/trends/${type}`)
-  const {amount, prevAmount} = await response.json()
-  return <BaseTrend type={type} amount={amount} prevAmount={prevAmount} />
+  const supabase = createClient()
+  let { data, error } = await supabase
+    .rpc('calculate_total', {
+      type_arg: type
+    })
+  if (error) throw new Error("Could not fetch the trend data")
+  const amount = data ?? 0
+
+  return <BaseTrend type={type} amount={amount} prevAmount={amount - 500} />
 }
--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,11 +1,13 @@
 import PageHeader from "@/components/page-header";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
   return (
-  <>
-    <PageHeader className="my-8" />
-    <main>{children}</main>
-    <footer className="mt-auto text-center py-8">Footer</footer>
-  </>
-  )
+    <>
+      <PageHeader className="my-8" />
+      <main>{children}</main>
+      <footer className="mt-auto py-8 text-center">
+        Footer
+      </footer>
+    </>
+  );
 }
