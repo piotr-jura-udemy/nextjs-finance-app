3a8bc84a61cc0f1fd7640855d572cba879f10d2e
Piotr Jura <jura.piotrek@gmail.com>
2024-02-28 17:51:24 +0100

    292-load-more

--- app/dashboard/components/transaction-list-wrapper.js
+++ app/dashboard/components/transaction-list-wrapper.js
@@ -3,5 +3,5 @@ import TransactionList from "./transaction-list"
 
 export default async function TransactionListWrapper({ range }) {
   const transactions = await fetchTransactions(range)
-  return <TransactionList initialTransactions={transactions} />
+  return <TransactionList initialTransactions={transactions} key={range} range={range} />
 }
--- app/dashboard/components/transaction-list.js
+++ app/dashboard/components/transaction-list.js
@@ -1,11 +1,26 @@
 'use client'
+import Button from "@/components/button"
 import Separator from "@/components/separator"
 import TransactionItem from "@/components/transaction-item"
 import TransactionSummaryItem from "@/components/transaction-summary-item"
+import { fetchTransactions } from "@/lib/actions"
 import { groupAndSumTransactionsByDate } from "@/lib/utils"
+import { useState } from "react"
 
-export default function TransactionList({ initialTransactions }) {
-  const grouped = groupAndSumTransactionsByDate(initialTransactions)
+export default function TransactionList({range, initialTransactions}) {
+  const [transactions, setTransactions] = useState(initialTransactions)
+  const [offset, setOffset] = useState(initialTransactions.length)
+  const grouped = groupAndSumTransactionsByDate(transactions)
+
+  const handleClick = async (e) => {
+    const nextTransactions = await fetchTransactions(range, offset, 10)
+    setOffset(prevValue => prevValue + 10)
+    setTransactions(prevTransactions => [
+      ...prevTransactions,
+      ...nextTransactions
+    ])
+  }
+
   return (
     <div className="space-y-8">
       {Object.entries(grouped)
@@ -20,6 +35,9 @@ export default function TransactionList({ initialTransactions }) {
             </section>
           </div>
         )}
+        <div className="flex justify-center">
+        <Button variant="ghost" onClick={handleClick}>Load More</Button>
+        </div>
     </div>
   )
 }
