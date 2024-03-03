cf20797d997dfc8db5a2efebf324f04fed95cc9b
Piotr Jura <jura.piotrek@gmail.com>
2024-03-03 13:17:07 +0100

    301-transaction-editing-suspense

---
+++ app/dashboard/transaction/[id]/edit/loading.js
@@ -0,0 +1,16 @@
+import Skeleton from "@/components/skeleton";
+
+export default function Loading() {
+  return <>
+    <h1 className="text-4xl font-semibold mb-8">Edit Transaction</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <Skeleton className="h-12" />
+      <Skeleton className="h-12" />
+      <Skeleton className="h-12" />
+      <Skeleton className="h-12" />
+      <Skeleton className="h-12" />
+      <Skeleton className="h-12" />
+      <Skeleton className="h-12 md:col-span-2" />
+    </div>
+  </>
+}
