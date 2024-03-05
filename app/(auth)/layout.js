24382c9b8ca02e4a1e0caeccaaa6af85a44752d8
Piotr Jura <jura.piotrek@gmail.com>
2024-03-05 19:15:00 +0100

    320-auth-layout-and-page

---
+++ app/(auth)/layout.js
@@ -0,0 +1,17 @@
+import { sizes, variants } from "@/lib/variants"
+import Link from "next/link"
+import {ChevronLeft} from "lucide-react"
+
+export default function Layout({children}) {
+  return <main>
+    <div className="absolute left-8 top-8">
+      <Link href="/" className={`${variants['ghost']} ${sizes['base']} flex items-center space-x-2 text-sm`}>
+        <ChevronLeft class="w-4 h-4" />
+        <span>Back</span>
+      </Link>
+    </div>
+    <div className="mt-8">
+      {children}
+    </div>
+  </main>
+}
---
+++ app/(auth)/login/page.js
@@ -0,0 +1,11 @@
+export default function Page() {
+  return <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] py-40">
+    <div className="flex flex-col space-y-8 text-center">
+      <h1 className="text-2xl font-semibold">Welcome back</h1>
+      <p className="text-sm text-gray-500 dark:text-gray-400">
+        Enter your email to sign in/create your account. No password is required.
+      </p>
+    </div>
+    <div>Form goes here...</div>
+  </div>
+}
