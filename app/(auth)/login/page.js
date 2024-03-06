081e4f101688354304af212a465134740db79952
Piotr Jura <jura.piotrek@gmail.com>
2024-03-06 19:10:36 +0100

    321-login-form

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -1,3 +1,5 @@
+import LoginForm from "./components/login-form";
+
 export default function Page() {
   return <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px] py-40">
     <div className="flex flex-col space-y-8 text-center">
@@ -6,6 +8,6 @@ export default function Page() {
         Enter your email to sign in/create your account. No password is required.
       </p>
     </div>
-    <div>Form goes here...</div>
+    <LoginForm />
   </div>
 }
