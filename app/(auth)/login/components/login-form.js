8cb8590076baccd6297800b107a853505a653381
Piotr Jura <jura.piotrek@gmail.com>
2024-03-07 17:55:46 +0100

    323-server-validation

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -2,13 +2,23 @@
 import Input from "@/components/input";
 import SubmitButton from "@/components/submit-button";
 import { login } from "@/lib/actions";
+import {useFormState} from "react-dom"
+
+const initialState = {
+  message: '',
+  error: false
+}
 
 export default function LoginForm() {
-  return <form action={login} className="space-y-2">
+  const [state, formAction] = useFormState(login, initialState)
+  return <form action={formAction} className="space-y-2">
     <Input type="email" placeholder="name@example.com"
       name="email" required />
     <SubmitButton type="submit" size="sm" className="w-full">
       Sign in with email
     </SubmitButton>
+    <p className={`${state?.error ? 'text-red-500' : 'text-green-500'} text-sm text-center`}>
+      {state?.message}
+    </p>
   </form>
 }
