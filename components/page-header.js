8862194f5710d1fab2c49f0269d3644c4aa3feb0
Piotr Jura <jura.piotrek@gmail.com>
2024-03-10 14:59:08 +0100

    329-authenticated-user

--- components/page-header.js
+++ components/page-header.js
@@ -1,16 +1,28 @@
 import Link from 'next/link'
 import DarkModeToggle from './dark-mode-toggle'
 import useServerDarkMode from '@/hooks/use-server-dark-mode'
+import { createClient } from '@/lib/supabase/server'
+import Button from './button'
+import {CircleUser, KeyRound} from 'lucide-react'
+import { sizes, variants } from '@/lib/variants'
 
-export default function PageHeader({className}) {
+export default async function PageHeader({className}) {
   const theme = useServerDarkMode()
+  const supabase = createClient()
+  const {data: {user}, error} = await supabase.auth.getUser()
   return (
     <header className={`flex justify-between items-center ${className}`}>
       <Link href="/dashboard" className="text-xl hover:underline underline-offset-8 decoration-2">Finance App</Link>
 
-      <div className="flex items-center space-x-4">
+      <div className="flex items-center">
         <DarkModeToggle defaultMode={theme} />
-        <div>User Dropdown</div>
+        {user && <Button variant="ghost" size="sm" className="flex items-center space-x-1">
+          <CircleUser className="w-6 h-6" />
+          <span>{user?.email}</span>  
+        </Button>}
+        {!user && <Link href="/login" className={`${variants['ghost']} ${sizes['sm']}`}>
+          <KeyRound className="w-6 h-6" />  
+        </Link>}
       </div>
     </header>
   )
