bd15a54a7ce026f65481d20c77825a9cbd77db30
Piotr Jura <jura.piotrek@gmail.com>
2024-03-11 19:05:05 +0100

    351-protecting-routes

--- middleware.js
+++ middleware.js
@@ -1,8 +1,17 @@
-import { NextResponse } from 'next/server'
 import { updateSession } from './lib/supabase/middleware'
+import { createClient } from './lib/supabase/server'
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(request) {
+  const { data: { user }} = await createClient().auth.getUser()
+
+  if (!user && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return Response.redirect(new URL('/login', request.url))
+  }
+
+  if (user && request.nextUrl.pathname.startsWith('/login')) {
+    return Response.redirect(new URL('/dashboard', request.url))
+  }
+
   return await updateSession(request)
 }
 
