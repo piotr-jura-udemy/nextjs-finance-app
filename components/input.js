2444185dd720872251fbba9ce6a7d49f3d9df0ef
Piotr Jura <jura.piotrek@gmail.com>
2024-03-15 13:28:33 +0100

    375-styling-file-input

--- components/input.js
+++ components/input.js
@@ -3,7 +3,8 @@ import { forwardRef } from "react"
 export default forwardRef(function Input(props, ref) {
   const styles = {
     'checkbox': 'rounded border-gray-300 text-gray-700 bg-white dark:bg-gray-950 dark:text-gray-500 shadow-sm disabled:opacity-75',
+    'file': 'file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:opacity-50 file:dark:text-gray-400',
     'default': 'w-full rounded-md shadow-sm border-gray-300 bg-white dark:border-gray-700 dark:bg-gray-950 disabled:opacity-75'
   }
-  return <input ref={ref} {...props} className={styles[props.type] ?? styles['default']} />
+  return <input ref={ref} {...props} className={`${styles[props.type] ?? styles['default']} ${props.className}`} />
 })
