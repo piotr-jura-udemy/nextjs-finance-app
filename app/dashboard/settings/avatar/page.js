11e051f650801ec4ab5b55ba6c52f66ff3186630
Piotr Jura <jura.piotrek@gmail.com>
2024-03-15 11:07:13 +0100

    374-file-upload-action

--- app/dashboard/settings/avatar/page.js
+++ app/dashboard/settings/avatar/page.js
@@ -1,13 +1,14 @@
 'use client'
 import Input from "@/components/input"
 import SubmitButton from "@/components/submit-button"
+import { uploadAvatar } from "@/lib/actions"
 
 export default function Page() {
   return <>
     <h1 className="text-4xl font-semibold mb-8">
       Avatar
     </h1>
-    <form className="space-y-4">
+    <form className="space-y-4" action={uploadAvatar}>
       <Input type="file" name="file" id="file" />
       <SubmitButton>Upload Avatar</SubmitButton>
     </form>
