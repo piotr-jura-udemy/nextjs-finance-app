0e43303c974e914aed37ea80d004ded3250e9045
Piotr Jura <jura.piotrek@gmail.com>
2024-03-15 10:32:51 +0100

    373-avatar-upload-form

--- app/dashboard/settings/avatar/page.js
+++ app/dashboard/settings/avatar/page.js
@@ -1,3 +1,15 @@
+'use client'
+import Input from "@/components/input"
+import SubmitButton from "@/components/submit-button"
+
 export default function Page() {
-  return (<>Avatar</>)
+  return <>
+    <h1 className="text-4xl font-semibold mb-8">
+      Avatar
+    </h1>
+    <form className="space-y-4">
+      <Input type="file" name="file" id="file" />
+      <SubmitButton>Upload Avatar</SubmitButton>
+    </form>
+  </>
 }
