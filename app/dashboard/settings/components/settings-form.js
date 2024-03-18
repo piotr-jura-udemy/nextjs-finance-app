8c3a0579c7b425d057b49dd406fa1b5a442cecc3
Piotr Jura <jura.piotrek@gmail.com>
2024-03-18 14:27:38 +0100

    validate server side

--- app/dashboard/settings/components/settings-form.js
+++ app/dashboard/settings/components/settings-form.js
@@ -2,6 +2,7 @@
 import AlertError from "@/components/alert-error";
 import AlertSuccess from "@/components/alert-success";
 import DateRangeSelect from "@/components/date-range-select";
+import FormError from "@/components/form-error";
 import Input from "@/components/input";
 import Label from "@/components/label";
 import SubmitButton from "@/components/submit-button";
@@ -11,6 +12,7 @@ import { useFormState } from "react-dom";
 const initialState = {
   message: "",
   error: false,
+  errors: {}
 };
 
 export default function SettingsForm({ defaults }) {
@@ -18,20 +20,24 @@ export default function SettingsForm({ defaults }) {
     updateSettings,
     initialState,
   );
+  console.log(state)
   return (
     <form className="space-y-4" action={formAction}>
       {state?.error && (
         <AlertError>{state?.message}</AlertError>
       )}
-      {!state?.error && state?.message.length > 0 && (
+      {!state?.error && state?.message?.length > 0 && (
         <AlertSuccess>{state?.message}</AlertSuccess>
       )}
 
       <Label htmlFor="fullName">User full name</Label>
       <Input type="text" name="fullName" id="fullName" placeholder="User full name" defaultValue={defaults?.fullName} />
+      
+      {state?.errors['fullName']?.map(error => <FormError error={error} key={`fullName-${error}`} />)}
 
       <Label htmlFor="defaultView">Default transactions view</Label>
       <DateRangeSelect name="defaultView" id="defaultView" defaultValue={defaults?.defaultView} />
+      <FormError error={state?.errors['defaultView']?.pop()} />
 
       <SubmitButton>Update Settings</SubmitButton>
     </form>
--- components/form-error.js
+++ components/form-error.js
@@ -1,3 +1,5 @@
 export default function FormError({ error }) {
-  return error && <p className="mt-1 text-red-500">{error.message}</p>
+  return error && <p className="mt-1 text-red-500">
+    {error}
+  </p>
 }
